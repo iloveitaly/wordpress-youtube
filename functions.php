63cde798a0b0f3bc424b4dffbdb67ae9ed830609
Michael Bianco <iloveitaly@gmail.com>
2015-03-16 21:01:13 -0400

    Extract first image from post if featured image isn't set

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ function iloveitaly_extract_youtube_image($content) {
 		return "";
 	}
 
-	return "http://your-generator.com/?quality=hq&play=true&input={$matches[1]}";
+	return "http://youtube-thumbnail-generator.herokuapp.com/?quality=hq&play=true&input={$matches[1]}";
 }
 
 function iloveitaly_youtube_embed($attrs) {
@@ -47,6 +47,10 @@ function iloveitaly_post_cover_image($post_id, $thumbnail_size = 'full') {
 	$cover_url = $cover['0'];
 
 	if(empty($cover_url)) {
+		$cover_url = iloveitaly_extract_first_image_in_post($post_id, $thumbnail_size);
+	}
+
+	if(empty($cover_url)) {
 		// TODO should request a minified version of the YT image from the server
 
 		$page = get_post($post_id);
@@ -56,3 +60,34 @@ function iloveitaly_post_cover_image($post_id, $thumbnail_size = 'full') {
 
 	return $cover_url;
 }
+
+function iloveitaly_extract_first_image_in_post($post_id, $thumbnail_size = 'full') {
+	$first_image = "";
+
+	// http://wordpress.stackexchange.com/questions/65502/getting-first-image-from-post
+	$post_attachments = get_children('post_parent='.$post_id.'&post_type=attachment&post_mime_type=image&order=asc');
+
+	if(!empty($post_attachments)) {
+		$files_keys = array_reverse(array_keys($post_attachments));
+		$first_attachment_in_post = wp_get_attachment_image_src($files_keys[0], $thumbnail_size);
+		$first_image = $first_attachment_in_post['0'];
+	}
+
+	if(empty($first_image)) {
+		$post = get_post($post_id);
+
+		// sometimes post_parent is nil; we have to resort to regex
+		// http://wordpress.stackexchange.com/questions/26958/extract-post-image-to-be-featured-images
+		preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+		$image_url = $matches[1][0];
+
+		// TODO try to extract the attachment reference from this and then pull the exact thumbnail size
+		
+		$first_image = $image_url;
+	}
+
+	return $first_image;
+}
+
+// TODO consider handling post orphans
+// http://wordpress.stackexchange.com/questions/114768/import-attachments-not-attaching-to-post-parent
